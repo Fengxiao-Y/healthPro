f8403d494b87dd23b26b77ed8b898cf74dd17333
Fengxiao-Y <1047792101@qq.com>
2023-09-07 16:31:09 +0800

    7牛云工具类封装

---
+++ health_parent/health_common/src/test/java/com/fx/test/QiNiuTest.java
@@ -0,0 +1,77 @@
+package com.fx.test;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
+import org.junit.Test;
+
+public class QiNiuTest {
+    /**
+     * 测试7牛图片上传
+     */
+    @Test
+    public void test1(){
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone2());
+//...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+//...生成上传凭证，然后准备上传
+        String accessKey = "3-_F7QRfJZl1GOXFNtlAJzU3Kd9_nG6C78eRl8Oc";
+        String secretKey = "oK2pfhmfFu9yq431WgdbjE66W1A3_0zC7ct9Pk5J";
+        String bucket = "yfx-health-space-1";
+//如果是Windows情况下，格式是 D:\\qiniu\\test.png
+        String localFilePath = "C:\\Users\\Laptop\\Desktop\\报销\\长沙交通费.jpg";
+//默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = "abc.jpg";
+//        String key = null;
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(localFilePath, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            System.out.println(putRet.key);
+            System.out.println(putRet.hash);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            System.err.println(r.toString());
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                //ignore
+            }
+        }
+    }
+
+    /**
+     * 删除7牛云服务器上的图片
+     */
+    @Test
+    public void test2(){
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+//...其他参数参考类注释
+
+        String accessKey = "3-_F7QRfJZl1GOXFNtlAJzU3Kd9_nG6C78eRl8Oc";
+        String secretKey = "oK2pfhmfFu9yq431WgdbjE66W1A3_0zC7ct9Pk5J";
+        String bucket = "yfx-health-space-1";
+
+        String key = "abc.jpg";
+
+        Auth auth = Auth.create(accessKey, secretKey);
+        BucketManager bucketManager = new BucketManager(auth, cfg);
+        try {
+            bucketManager.delete(bucket, key);
+        } catch (QiniuException ex) {
+            //如果遇到异常，说明删除失败
+            System.err.println(ex.code());
+            System.err.println(ex.response.toString());
+        }
+    }
+}
